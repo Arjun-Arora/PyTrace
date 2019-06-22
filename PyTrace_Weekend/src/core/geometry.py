93e3e65d592acaa248f7ca5ae1bb34eb4d889568
Arjun Arora <aarora52@stanford.edu>
2019-06-21 19:12:00 -0700

    added to vec3 class outstanding tests: idiv imul

--- PyTrace_Weekend/src/core/geometry.py
+++ PyTrace_Weekend/src/core/geometry.py
@@ -5,19 +5,77 @@ class vec3:
         self.x0 = x0
         self.x1 = x1
         self.x2 = x2
-        self.x = x0
-        self.y = x1
-        self.z = x2
-
-        self.r = x0
-        self.g = x1
-        self.b = x2
     def __str__(self) -> str:
         return "[ {} {} {} ]".format(self.x0,self.x1,self.x2)
     def __add__(self,other):
         return  vec3(self.x0 + other.x0, self.x1 + other.x1, self.x2 + other.x2)
+    def __iadd__(self,other):
+        self.x0 = other.x0 + self.x0
+        self.x1 = other.x1 + self.x1
+        self.x2 = other.x2 + self.x2
+        return self
     def __sub__(self,other):
         return vec3(self.x0 - other.x0, self.x1 - other.x1, self.x2 - other.x2)
+    def __isub__(self,other):
+        self.x0 = -other.x0 + self.x0
+        self.x1 = -other.x1 + self.x1
+        self.x2 = -other.x2 + self.x2
+        return self
+    def __mul__(self,other):
+        if isinstance(other,vec3):
+            return vec3(self.x0 * other.x0, self.x1 * other.x1, self.x2 * other.x2)
+        elif isinstance(other,float) or isinstance(other,int) or isinstance(other,long):
+            return vec3(self.x0 * other, self.x1 * other, self.x2 * other)
+        else:
+            assert(False,"cannot multiply vec3 by a non numeric class or non vec3")
+    def __imul__(self,other):
+        if isinstance(other,vec3):
+            self.x0 = other.x0 * self.x0
+            self.x1 = other.x1 * self.x1
+            self.x2 = other.x2 * self.x2
+            return self
+        elif isinstance(other,float) or isinstance(other,int) or isinstance(other,long):
+            self.x0 = x0 * other
+            self.x1 = x1 * other
+            self.x2 = x2 * other
+            return self
+        else:
+            assert(False,"cannot multiply vec3 by a non numeric class or non vec3")
+    def __truediv__(self,other):
+        if isinstance(other,vec3):
+            return vec3(self.x0 / other.x0, self.x1 / other.x1, self.x2 / other.x2)
+        elif isinstance(other,float) or isinstance(other,int) or isinstance(other,long):
+            return vec3(self.x0 / other, self.x1 / other, self.x2 / other)
+        else:
+            assert(False,"cannot divide vec3 by a non numeric class or non vec3")
+    def __idiv__(self,other):
+        if isinstance(other,vec3):
+            self.x0 = self.x0 / other.x0 
+            self.x1 = self.x1 / other.x1
+            self.x2 = self.x2 / other.x2
+            return self
+        elif isinstance(other,float) or isinstance(other,int) or isinstance(other,long):
+            self.x0 = x0 / other
+            self.x1 = x1 / other
+            self.x2 = x2 / other
+            return self
+        else: 
+            assert(False,"cannot divide vec3 by a non numeric class or non vec3")
+    def __neg__(self):
+        return vec3(-self.x0,-self.x1,-self.x2)
+    def __eq__(self,other):
+        return ((self.x0 == other.x0) and (self.x1 == other.x1) and (self.x2 == other.x2))
+    def __ne__(self,other):
+        return not(self == other)
+    def __pow__(self,other: float):
+        return vec3(self.x0 ** other, self.x1 ** other,self.x2 ** other)
+    def __ipow__(self,other: float):
+        self.x0 = self.x0 ** other
+        self.x1 = self.x1 ** other
+        self.x2 = self.x2 ** other
+        return self
+    def __len__(self):
+        return sqrt(self.x0 * self.x0 + self.x1 * self.x1 + self.x2 * self.x2)
 
 
 
--- PyTrace_Weekend/src/core/geometry_test.py
+++ PyTrace_Weekend/src/core/geometry_test.py
@@ -1,15 +1,109 @@
-import geometry 
+from geometry import *
 
 def vec3_basic_add_test():
-    a = geometry.vec3(3,4,6)
-    b = geometry.vec3(2,2,1)
-    c =  a + b
-    print(a)
-    print("\n + \n")
-    print(b)
-    print("\n = \n")
-    print(c)
+	print("vec3_basic_add_test")
+	a = vec3(3,4,6)
+	b = vec3(2,2,1)
+	c =  a + b
+	if c == vec3(5,6,7):
+		print("success!\n")
+		return 1
+	else:
+		print("failed\n")
+		return 0 
 
+def vec3_basic_subtract_test():
+	print("vec3_basic_subtract_test")
+	a = vec3(3,4,6)
+	b = vec3(4,4,7)
+	c =  a - b
+	if c == vec3(-1,0,-1):
+		print("success!\n")
+		return 1
+	else:
+		print("failed\n")
+		return 0 
+def vec3_basic_negation_test():
+	print("vec3_basic_negation_test")
+	a = vec3(3,4,6)
+	if -a == vec3(-3,-4,-6):
+		print("success!\n")
+		return 1 
+	else:
+		print("failed\n")
+		return 0 
+def vec3_basic_iadd_test():
+	print("vec3_basic_iadd_test")
+	a = vec3(3,4,6)
+	a += vec3(3,2,1)
+	if (a == vec3(6,6,7)):
+		print("success!\n")
+		return 1
+	else:
+		print("failed\n")
+		return 0 
+def vec3_basic_isub_test():
+	print("vec3_basic_isub_test")
+	a = vec3(3,4,6)
+	a -= vec3(3,2,1)
+	if (a == vec3(0,2,5)):
+		print("success!\n")
+		return 1 
+	else:
+		print("failed\n")
+		return 0 
+def vec3_basic_mult_test():
+	print("vec3_basic_mult_test")
+	a = vec3(1,2,3)
+	b = vec3(2,2,2)
+	if a * 2 == a * b:
+		print("success!\n")
+		return 1
+	else:
+		print("failed\n")
+		return 0 
+def vec3_basic_div_test():
+	print("vec3_basic_div_test")
+	a = vec3(1,2,3)
+	b = vec3(2,2,2)
+	if a / 2 == a / b:
+		print("success!\n")
+		return 1
+	else:
+		print("failed\n")
+		return 0 
+def vec3_basic_neq_test():
+	print("vec3_basic_neq_test")
+	a = vec3(3,4,6)
+	b = vec3(3,4,7)
+	if (a != b and not(a != a)):
+		print("success!\n")
+		return 1 
+	else:
+		print("failed\n")
+		return 0
+def vec3_basic_pow_test():
+	print("vec3_basic_pow_test")
+	a = vec3(2,4,6)
+	b = 2
+	c = vec3( 4,16,36)
+	diff = (a ** b) - c 
+	if diff.x0 <= 1e-6 and diff.x1 <= 1e-6 and diff.x2 <= 1e-6:
+		print("success!\n")
+		return 1
+	else:
+		print("failure\n")
+		return 0 
 if __name__== "__main__":
-    vec3_basic_add_test()
+	vec3_basic_add_test()
+	vec3_basic_subtract_test()
+	vec3_basic_negation_test()
+	vec3_basic_iadd_test()
+	vec3_basic_isub_test()
+	vec3_basic_mult_test()
+	vec3_basic_div_test()
+	vec3_basic_neq_test()
+	vec3_basic_pow_test()
+
+
 
