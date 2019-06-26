3e8627cee756f903f8e15de0e763ee0a48a5b2d4
Arjun Arora <aarora52@stanford.edu>
2019-06-26 01:43:54 -0700

    refactored some things
    moved coordinates and vector creation to utils
    
    might want to rename but we will see

--- PyTrace_Weekend_numpy/main.py
+++ PyTrace_Weekend_numpy/main.py
@@ -8,24 +8,24 @@ import matplotlib.pyplot as plt
 def color(r: ray):
 	#print(r.direction)
 	unit_direction = unit_vector(r.direction())
-	t = 0.5 * (unit_direction[:,:,1]) + 1.0
-	return (1.0-t)[:,:,None] * np.array((1.0,1.0,1.0))[None,None,:] + t[:,:,None] * np.array((0.5,0.7,1.0))[None,None,:]
+	t = coordinatize(0.5 * (unit_direction[:,:,1]) + 1.0)
+	return (1.0-t) * vectorize(np.array((1.0,1.0,1.0))) + t * vectorize(np.array((0.5,0.7,1.0)))
 
-def main(nx: float = 200, ny: float = 100):
+def main(nx: float = 1200, ny: float = 800):
 	j = np.tile(np.arange(0,ny,1),reps=(nx,1))
 	i = np.tile(np.arange(0,nx,1),reps=(ny,1)).T
 
-	lower_left_corner = np.array((-2.0,-1.0,-1.0))[None,None,:]
-	horizontal = np.array((4.0,0,0))[None,None,:]
-	vertical = np.array((0.0,2,0))[None,None,:]
-	origin = np.array((0.0,0.0,0.0))[None,None,:]
+	lower_left_corner = vectorize(np.array((-2.0,-1.0,-1.0)))
+	horizontal = vectorize(np.array((4.0,0,0)))
+	vertical = vectorize(np.array((0.0,2,0)))
+	origin = vectorize(np.array((0.0,0.0,0.0)))
 
 	# print(i.shape)
 	# print(j)
-	u = i/nx
-	v = j /ny 
+	u = coordinatize(i/nx)
+	v = coordinatize(j/ny)
 
-	r = ray(origin,lower_left_corner + u[:,:,None] * horizontal + v[:,:,None] * vertical)
+	r = ray(origin,lower_left_corner + u * horizontal + v * vertical)
 	col = color(r)
 
 	#b = np.ones_like(r) * 0.2 
--- PyTrace_Weekend_numpy/src/core/utils.py
+++ PyTrace_Weekend_numpy/src/core/utils.py
@@ -4,6 +4,16 @@ import numpy as np
 functions here will be misc functions useful in other locations
 """
 
+def coordinatize(arr):
+	'''
+	converts coordinate array of mxn to be m x n x 1
+	'''
+	return arr[:,:,None]
+def vectorize(vector):
+	'''
+	converts vector of 3, to be of 1 x 1 x 3
+	'''
+	return vector[None,None,:]
 def unit_vector(vector):
 	'''
 	normalizes all 3-vectors in an array to be unit vectors
@@ -11,9 +21,9 @@ def unit_vector(vector):
 	assert(vector.shape[-1]) == 3
 
 	length = (vector ** 2).sum(axis=-1) ** 0.5
-
+	print(vector.shape)
 	length = np.repeat(length[:,:,np.newaxis],3,axis=2)
-	#print(length.shape)
+	print(length.shape)
 	return_val = vector / length
 	#print(return_val.shape)
 	return return_val
