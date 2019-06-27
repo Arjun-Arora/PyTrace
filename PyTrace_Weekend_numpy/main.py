ce391be35a28d17983194bba0c94b80b1252223d
Arjun Arora <aarora52@stanford.edu>
2019-06-27 15:43:57 -0700

    fixed chapter 5 bug
    will now work forward from here

--- PyTrace_Weekend_numpy/main.py
+++ PyTrace_Weekend_numpy/main.py
@@ -21,26 +21,26 @@ def color(r: ray,world: list,tile_shape,depth = 0,max_depth = 4):
 	target = 0
 	tile_x,tile_y = tile_shape
 	hit_color = tile(np.zeros((tile_x,tile_y,3)))
-	hit_anything,rec_list = iterate_hit_list(r,tile(np.ones((tile_x,tile_y)) * 0.01),
-							tile(np.ones((tile_x,tile_y)) * MAX_FLOAT),world)
-	if hit_anything and depth <= max_depth:
-		for rec in rec_list:
-			scattered = None
-			attenuation = None
-			if_scatter,(scattered,attenuation) = rec.mat.scatter(r,rec)
-			if if_scatter:
-			#masks out all values that we didn't get hits for 
-				mask = ~np.all(rec.t == -1.0, axis=-1)
-				t += tile(mask) * rec.t
-				#target = rec.p + rec.normal + tile(mask) * random_in_unit_sphere(rec.normal)
-				#mask out all color contributions that are calculated incorrectly using a mask
-				hit_color += 0.5 * tile(mask)  * attenuation *  color(scattered,world,tile_shape,depth+1,max_depth)
+	hit_anything,rec_list,closest_hit = iterate_hit_list(r,tile(np.ones((tile_x,tile_y)) * 0.01),
+										tile(np.ones((tile_x,tile_y)) * MAX_FLOAT),world)
+	# if hit_anything and depth <= max_depth:
+	# 	for rec in rec_list:
+	# 		scattered = None
+	# 		attenuation = None
+	# 		if_scatter,(scattered,attenuation) = rec.mat.scatter(r,rec)
+	# 		if if_scatter:
+	# 		#masks out all values that we didn't get hits for 
+	# 			mask = ~np.all(rec.t == -1.0, axis=-1)
+	# 			t += tile(mask) * rec.t
+	# 			#target = rec.p + rec.normal + tile(mask) * random_in_unit_sphere(rec.normal)
+	# 			#mask out all color contributions that are calculated incorrectly using a mask
+	# 			hit_color += 0.5 * tile(mask) * (closest_hit == rec.t) * attenuation *  color(scattered,world,tile_shape,depth+1,max_depth)
 
-	# for record in rec_list:
-	# 	N = record.normal
-	# 	mask = ~np.all(record.t == -1.0, axis=-1)
-	# 	t += tile(mask) * record.t
-	# 	hit_color += tile(mask) * vec3(N[:,:,0] + 1,N[:,:,1] + 1,N[:,:,2] + 1) * 0.5
+	for record in rec_list:
+		N = record.normal
+		mask = ~np.all(record.t == -1.0, axis=-1)
+		t += tile(mask) * record.t
+		hit_color += tile(mask) * (closest_hit == record.t) * vec3(N[:,:,0] + 1,N[:,:,1] + 1,N[:,:,2] + 1) * 0.5
 
 	
 	unit_direction = unit_vector(r.direction())
@@ -53,9 +53,10 @@ def main(nx: float = 200, ny: float = 100,ns: float = 100):
 	i = np.tile(np.arange(0,nx,1),reps=(ny,1)).T
 
 	hit_object_list = []
-	hit_object_list.append(sphere(vec3(0,0,-1),0.5,lambertian(vec3(0.8,0.3,0.3))))
-	hit_object_list.append(sphere(vec3(-1,0,-1),0.5,metal(vec3(0.8,0.8,0.8))))
 	hit_object_list.append(sphere(vec3(0,-100.5,-1),100,lambertian(vec3(0.8,0.8,0.0))))
+	hit_object_list.append(sphere(vec3(0,0,-1),0.5,lambertian(vec3(0.8,0.3,0.3))))
+	#it_object_list.append(sphere(vec3(-1,0,-1),0.5,metal(vec3(0.8,0.8,0.8))))
+	
 
 	#hit_object_list.append(sphere(vec3(1,0,-1),0.5,lambertian(vec3(0.8,0.6,0.2))))
 
@@ -72,14 +73,14 @@ def main(nx: float = 200, ny: float = 100,ns: float = 100):
 			pbar.update(1)
 
 	col /= float(ns)
-	ir = 255.99 * np.sqrt(col[:,:,0])
-	ig = 255.99 * np.sqrt(col[:,:,1])
-	ib = 255.99 * np.sqrt(col[:,:,2])
+	ir = 255.99 * col[:,:,0]
+	ig = 255.99 * col[:,:,1]
+	ib = 255.99 * col[:,:,2]
 	output = np.dstack((ir,ig,ib))
 	# print(output.shape)
 	plt.imsave("output.png",np.rot90(output.astype(int)))
 if __name__ == "__main__": 
-	main(200,100,10)
+	main(200,100,100)
 	# X = random_in_unit_sphere(np.random.randn(200,100,3))
 	# fig = plt.figure()
 	# ax = fig.add_subplot(111)
--- PyTrace_Weekend_numpy/src/core/hitable.py
+++ PyTrace_Weekend_numpy/src/core/hitable.py
@@ -42,8 +42,9 @@ def iterate_hit_list(r:ray, t_min:float, t_max: float, object_list: list):
 		if hit_object:
 			hit_anything = True
 			closest_hit = np.minimum(np.where(temp_rec.t > 0,temp_rec.t,MAX_FLOAT),closest_hit)
+			#closest_hit = np.minimum(temp_rec.t, closest_hit)
 			rec_list.append(temp_rec)
 	# print(hit_anything)
-	return hit_anything,rec_list
+	return hit_anything,rec_list,closest_hit
 
 
